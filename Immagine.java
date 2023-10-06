bbcaa61a44536bfa7d234305e973f45c5d92811c
flaviodeangelis56 <deangelisflavio@gmail.com>
2023-10-06 12:38:46 +0200

    add-immagine-class

---
+++ Immagine.java
@@ -0,0 +1,66 @@
+public class Immagine {
+    public String titolo;
+    public Luminosità luminosità;
+
+    public Immagine(String titolo, Luminosità luminosità) {
+        this.titolo = titolo;
+        this.luminosità = luminosità;
+    }
+
+    public void alzaLuminosità() {
+        if (this.luminosità == Luminosità.CINQUE) {
+            System.out.println("La luminosità è al massimo");
+        }
+        if (this.luminosità == Luminosità.QUATTRO) {
+            this.luminosità = Luminosità.CINQUE;
+        }
+        if (this.luminosità == Luminosità.TRE) {
+            this.luminosità = Luminosità.QUATTRO;
+        }
+        if (this.luminosità == Luminosità.DUE) {
+            this.luminosità = Luminosità.TRE;
+        }
+        if (this.luminosità == Luminosità.UNO) {
+            this.luminosità = Luminosità.DUE;
+        }
+    }
+
+    public void abassaLuminosità() {
+        if (this.luminosità == Luminosità.UNO) {
+            System.out.println("La luminosità è al minimo");
+        }
+        if (this.luminosità == Luminosità.DUE) {
+            this.luminosità = Luminosità.UNO;
+        }
+        if (this.luminosità == Luminosità.TRE) {
+            this.luminosità = Luminosità.DUE;
+        }
+        if (this.luminosità == Luminosità.QUATTRO) {
+            this.luminosità = Luminosità.TRE;
+        }
+        if (this.luminosità == Luminosità.CINQUE) {
+            this.luminosità = Luminosità.QUATTRO;
+        }
+    }
+
+    public void show() {
+        if (this.luminosità == Luminosità.UNO) {
+            System.out.println(this.titolo + "*");
+        }
+        if (this.luminosità == Luminosità.DUE) {
+            System.out.println(this.titolo + "**");
+        }
+
+        if (this.luminosità == Luminosità.TRE) {
+            System.out.println(this.titolo + "***");
+        }
+        if (this.luminosità == Luminosità.QUATTRO) {
+            System.out.println(this.titolo + "****");
+        }
+        if (this.luminosità == Luminosità.CINQUE) {
+            System.out.println(this.titolo + "*****");
+        }
+    }
+}
+
+
--- MainRiproduci.java
+++ MainRiproduci.java
@@ -2,7 +2,8 @@ public class MainRiproduci {
     public static void main(String[] args) {
         Audio audio1 = new Audio("test1", 2, Volume.QUATTRO);
         Video video1 = new Video("video1", 4, Volume.TRE, Luminosità.TRE);
+        Immagine immagine1 = new Immagine("immagine1", Luminosità.QUATTRO);
 
-
+        
     }
 }
